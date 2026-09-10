21ca96acbbbceff07c790723836b24406ee7f132
KarlaBassfeld <karla.bassfeld@escola.pr.gov.br>
2026-09-10 20:12:00 -0300

    10/10

--- main.js
+++ main.js
@@ -23,36 +23,37 @@ function aumentaTamanho() {
 }
 
 const campoSenha = document.querySelector('#campo-senha');
-const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const checkbox = document.querySelectorAll('.checkbox');
 
-for (i= 0; i < checkbox.length; i++) {
+for (i = 0; i < checkbox.length; i++) {
     checkbox[i].onclick = geraSenha;
 }
 
-const letrasMaiusculas ='ABCDFGHIJKKLMNOPQRSTUVWXYZ';
-const letraMinuscula = 'abcdefghijklmnopqrstuvwxyz';
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
 const numeros = '0123456789';
 const simbolos = '!@%*?';
 geraSenha();
 
-function geraSenha() 
+function geraSenha() {
     let alfabeto = '';
-            if (checkbox[0].checked) {
+    if (checkbox[0].checked) {
         alfabeto = alfabeto + letrasMaiusculas;
-}
+    }
     if (checkbox[1].checked) {
-        alfabeto = alfabeto + letraMinuscula
-}
+        alfabeto = alfabeto + letrasMinusculas;
+    }
     if (checkbox[2].checked) {
         alfabeto = alfabeto + numeros;
-}
+    }
     if (checkbox[3].checked) {
         alfabeto = alfabeto + simbolos;
-}
+    }
     let senha = '';
     for(let i=0; i< tamanhoSenha; i++) {
-        let numeroAleatorio = Math.random() * letrasMaiusculas.length;
+        let numeroAleatorio = Math.random() * alfabeto.length;
         numeroAleatorio = Math.floor(numeroAleatorio);
-        senha = senha + letrasMaiusculas[numeroAleatorio];
+        senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha;
+}
